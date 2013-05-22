7899bf9038641ec8116cadb4f8fe6b9ca8db1f95
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-05-22 17:58:48 +0200

    read and added TODOs referring to the problems faced in the naive Minimum.v

--- coq/Minimum.v
+++ coq/Minimum.v
@@ -4,43 +4,48 @@ Require Import Coq.Arith.Compare_dec.
 
 Open Scope nat_scope.
 
-Inductive tree := 
-  | Tip : tree
-  | Bin : nat -> tree -> tree -> tree.
-
-Definition ht (t : tree) :=
-  match t with
-  | Tip       => 0
-  | Bin n _ _ => n
-  end.
 
 Definition max (a b : nat) :=
-  match nat_compare a b with
-  | Lt => b
-  | _  => a
-  end.
+    match nat_compare a b with
+    | Lt => b
+    | _  => a
+    end.
 
-Definition join (x y : tree) := 
-  Bin (max (ht x) (ht y)) x y.
 
-Fixpoint step (t : tree) (xs : list tree) := 
-  match xs with
-  | nil => t :: nil
-  | u :: nil => 
-      match nat_compare (ht t) (ht u) with
-      | Lt => t :: u :: nil
-      | _  => (join t u) :: nil
-      end
-  | u :: v :: ts =>
-      match nat_compare (ht t) (ht u) with
-      | Lt => t :: u :: v :: ts
-      | _  => 
-          match nat_compare (ht t) (ht v) with
-          | Lt => step (join t u) (v :: ts)
-          | _  => step t (step (join u v) ts)
-          end
-      end
-  end.
+Inductive tree :=
+    | Tip : tree
+    | Bin : nat -> tree -> tree -> tree.
 
-Definition build (xs : list tree) := 
-  fold_left join (fold_right step nil xs).
+Definition ht (t : tree) :=
+    match t with
+    | Tip       => 0
+    | Bin n _ _ => n
+    end.
+
+
+(* TODO: should increment height *)
+Definition join (x y : tree) := Bin (max (ht x) (ht y)) x y.
+
+Fixpoint step (t : tree) (xs : list tree) :=
+    match xs with
+    | nil      => t :: nil
+    | u :: nil =>
+        match nat_compare (ht t) (ht u) with
+        | Lt => t :: u :: nil
+        | _  => (join t u) :: nil
+        end
+    | u :: v :: ts =>
+        match nat_compare (ht t) (ht u) with
+        | Lt => t :: u :: v :: ts
+        | _  =>
+            match nat_compare (ht t) (ht v) with
+            | Lt => step (join t u) (v :: ts)  (* non-structural recursion? *)
+            | _  => step t (step (join u v) ts)  (* non-structural recursion *)
+            end
+        end
+    end.
+
+
+(* TODO: problem: there is no fold1 is Coq (partial) *)
+Definition build (xs : list tree) := fold_left join (fold_right step nil xs).
+
