f942f726460adc96b1c67fdf56318e44b7b943ef
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-06-13 15:20:41 +0200

    quicksort bove-capretta proof working, next implement quicksort itself

--- coq/QS.v
+++ coq/QS.v
@@ -1,6 +1,23 @@
 Require Import Coq.Lists.List.
 Require Import Coq.Numbers.Natural.Peano.NPeano.
+Require Import Coq.Bool.Bool.
 
-Inductive Qs (x : nat) (l : list nat) : Prop :=
-  | qs_nil  : Qs x nil
-  | qs_cons : Qs x (filter (fun y => ltb y x) l) -> Qs x (filter (fun y => not (leb y x)) l) -> Qs x l.
+Open Scope List_Scope.
+
+
+Inductive QS : list nat -> Prop :=
+    | qs_nil  : QS nil
+    | qs_cons : forall (x : nat) (xs : list nat),
+                    QS (filter (fun y => ltb y x) xs) ->
+                    QS (filter (fun y => negb (ltb y x)) xs) ->
+                    QS (x :: xs).
+
+
+Fixpoint quicksort (qs_acc : QS) (l : list nat) : list nat :=
+    match qs_acc, l with
+    | qs_nil, _ => []
+    | (qs_cons lesser greater), x :: xs =>
+            quicksort lesser (filter (fun y => ltb y x) xs)
+            ++ [x]
+            ++ quicksort greater (filter (fun y => negb (ltb y x)) xs)
+    end.
