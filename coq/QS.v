e8fe1423321afe647c9ef28ec8cad2c5ad168618
Liewe Thomas van Binsbergen <thomas@ltpro.(none)>
2013-06-17 11:50:58 +0200

    Defined QuickSort, and proved termination for a list. However, can not seem to be able to actually obtain the sorted value

--- coq/QS.v
+++ coq/QS.v
@@ -5,19 +5,30 @@ Require Import Coq.Bool.Bool.
 Open Scope list_scope.
 
 
-Inductive QS : list nat -> Prop :=
+Inductive QS : list nat -> Type :=
     | qs_nil  : QS nil
-    | qs_cons : forall (x : nat) (xs : list nat),
+    | qs_cons : forall {x : nat} {xs : list nat},
                     QS (filter (fun y => ltb y x) xs) ->
                     QS (filter (fun y => negb (ltb y x)) xs) ->
                     QS (x :: xs).
 
 
-Fixpoint quicksort (qs_acc : QS) (l : list nat) : list nat :=
-    match qs_acc, l with
-    | qs_nil, _ => []
-    | (qs_cons lesser greater), x :: xs =>
-            quicksort lesser (filter (fun y => ltb y x) xs)
-            ++ [x]
-            ++ quicksort greater (filter (fun y => negb (ltb y x)) xs)
+Fixpoint quicksort (l : list nat)  (qs_acc : QS l) : list nat :=
+    match qs_acc with
+    | qs_nil  => nil
+    | (qs_cons p ps lesser greater) =>
+            quicksort (filter (fun y => ltb y p) ps) lesser
+            ++ p :: nil
+            ++ quicksort (filter (fun y => negb (ltb y p)) ps) greater
     end.
+
+Definition list := 5::3::1::4::nil.
+
+Theorem exTerm : QS list.
+Proof.
+  apply qs_cons. apply qs_cons. apply qs_cons. simpl. apply qs_nil. simpl. apply qs_nil. apply qs_cons; simpl; apply qs_nil.
+  simpl. apply qs_nil.
+Qed.
+
+Eval simpl in filter (fun y => ltb y 5) list.
+Eval simpl in quicksort list exTerm.
